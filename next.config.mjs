66136e07958cbe71074820276b9ae0b8f08adad5
hisana-shah <hisananazeer1@gmail.com>
2025-07-27 21:52:15 +0530

    debug login

--- next.config.mjs
+++ next.config.mjs
@@ -9,10 +9,16 @@ module.exports = {
         source: "/(.*)", // all routes
         headers: [
           {
-            key: "Content-Security-Policy",
-            value:
-              "default-src 'self'; connect-src 'self' https://*.firebaseio.com https://*.googleapis.com https://*.gstatic.com; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://*.firebaseio.com https://*.googleapis.com https://*.gstatic.com;",
-          },
+  key: "Content-Security-Policy",
+  value: `
+    default-src 'self';
+    script-src 'self' https://www.gstatic.com https://www.googleapis.com;
+    connect-src 'self' https://www.googleapis.com https://firebase.googleapis.com https://*.firebaseio.com;
+    img-src 'self' data:;
+    style-src 'self' 'unsafe-inline';
+  `.replace(/\n/g, ""),
+}
+
         ],
       },
     ];
